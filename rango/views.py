8b6c626cfc8f997e9f8c74d34a3a1f6552fcd691
“Marilena <2604912c@student.gla.ac.uk>
2021-07-29 16:55:21 +0300

    fixed and added the correct things

--- rango/views.py
+++ rango/views.py
@@ -3,12 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    
-    return HttpResponse("Rango says hey there partner!", 'rango/index.html')
+    return HttpResponse("Rango says hey there partner!")
 
 
-def about(request):
-
-    return HttpResponse("Rango says here is the about page.", 'rango/about.html')
-    
+# def about(request):
+#     return HttpResponse("Rango says here is the about page. <a href ='http://127.0.0.1:8000/'>About</a>")
+#<a href ='http://127.0.0.1:8000/rango/about/'>About</a>
     
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from rango import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
