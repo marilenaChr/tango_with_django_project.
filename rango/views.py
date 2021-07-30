02fb65c87b798046615a458fa19417908002f306
“Marilena <2604912c@student.gla.ac.uk>
2021-07-30 14:45:56 +0300

    add reverse

--- rango/views.py
+++ rango/views.py
@@ -48,7 +48,7 @@ def add_category(request):
 
         if form.is_valid():
             cat = form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
 
@@ -61,7 +61,7 @@ def add_page(request, category_name_slug):
         category = None
     
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
